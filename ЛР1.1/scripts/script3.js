90593a0895d3e28c02fc3b1d0043815bf96b3566
R0stislaw <115527575+R0stislaw@users.noreply.github.com>
2023-10-11 15:12:44 +0300

    Add files via upload

--- ЛР1.1/scripts/script3.js
+++ ЛР1.1/scripts/script3.js
@@ -1,18 +1,22 @@
 var monthInput = prompt("Введіть номер місяця від 1 до 12:");
 if (/^[1-9]|1[0-2]$/.test(monthInput)) {
   var monthNumber = parseInt(monthInput);
+  console.log(monthNumber);
   switch (true) {
-    case monthNumber >= 3 && monthNumber <= 5:
+    case monthInput >= 3 && monthInput <= 5:
       alert(`Пора року - Весна`);
       break;
-    case monthNumber >= 6 && monthNumber <= 8:
+    case monthInput >= 6 && monthInput <= 8:
       alert(`Пора року - Літо`);
       break;
-    case monthNumber >= 9 && monthNumber <= 11:
+    case monthInput >= 9 && monthInput <= 11:
       alert(`Пора року - Осінь`);
       break;
-    default:
+    case monthInput <= 2 || monthInput == 12:
       alert(`Пора року - Зима`);
+      break;
+    default:
+      alert(`Введіть номер місяця від 1 до 12`);
   }
 } else {
   alert("Некоректний номер місяця. Введіть число від 1 до 12.");
